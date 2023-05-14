f9055c98c15d0c8d7cff29a1d1410a9becb5c9d0
benamager <benaprivat@gmail.com>
2023-05-14 19:46:46 +0200

    Add delete prompt from frontend

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -28,8 +28,24 @@ export default function MyProfile() {
     router.push(`/update-prompt?id=${post._id}`)
   }
 
-  async function handleDelete() {
+  async function handleDelete(post) {
+    const hasConfimed = confirm("Are you sure you want to delete this prompt?")
 
+    if (hasConfimed) {
+      try {
+        // Delete prompt from database via API route
+        await fetch(`/api/prompt/${post._id}`, {
+          method: 'DELETE'
+        })
+
+        // Remove prompt from state
+        const filteredPosts = posts.filter((p) => p._id !== post._id)
+        setPosts(filteredPosts)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 
   return (
