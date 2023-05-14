91a82c15e24a7343407b4afa77820a9127a0616b
benamager <benaprivat@gmail.com>
2023-05-14 16:03:28 +0200

    Add create prompt route and fix bugs in page

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,14 +7,38 @@ import { useRouter } from "next/navigation";
 import Form from "@/components/Form";
 
 export default function CreatePrompt() {
+  const router = useRouter()
   const [submitting, setSubmitting] = useState(false)
+  const { data: session } = useSession()
   const [post, setPost] = useState({
     prompt: '',
     tag: ''
   })
 
   async function createPrompt(e) {
+    e.preventDefault()
+    setSubmitting(true)
 
+    try {
+      // Post new prompt to database via API route
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+          userId: session?.user.id
+        }),
+      })
+
+      if (response.ok) {
+        router.push('/')
+      }
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setSubmitting(false)
+    }
   }
 
   return (
